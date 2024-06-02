9f7ac236064d8a4925f452e4cd6fadde46b875d0
Full Name <theofficialwebsiteguys@gmail.com>
2024-06-02 14:04:49 -0400

    heroku server

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -44,98 +44,98 @@ export class HeroComponent {
       this.isHomePage = false;
       switch (url) {
         case '/nike':
-          this.backgroundImage = '../../assets/nike-hero.jpg';
+          this.backgroundImage = 'assets/nike-hero.jpg';
           this.page_title = 'Nike';
           this.heroHeight = '30vh';
           break;
         case '/jordan':
-          this.backgroundImage = '../../assets/jordan-hero.jpg';
+          this.backgroundImage = 'assets/jordan-hero.jpg';
           this.page_title = 'Jordan';
           this.heroHeight = '30vh';
           break;
         case '/yeezy':
-          this.backgroundImage = '../../assets/yeezy-hero.jpg';
+          this.backgroundImage = 'assets/yeezy-hero.jpg';
           this.page_title = 'Yeezy';
           this.heroHeight = '30vh';
           break;
         case '/clothing/essentials':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Essentials';
           this.heroHeight = '30vh';
           break;
         case '/clothing/denim-tears':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Denim Tears';
           this.heroHeight = '30vh';
           break;
         case '/clothing/bape':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Bape';
           this.heroHeight = '30vh';
           break;
         case '/clothing/eric-emanuel':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Eric Emanuel';
           this.heroHeight = '30vh';
           break;
         case '/clothing/hellstar':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Hellstar';
           this.heroHeight = '30vh';
           break;
         case '/clothing/pharaoh-collection':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Pharaoh Collections';
           this.heroHeight = '30vh';
           break;
         case '/clothing/limited-hype':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'Limited Hype';
           this.heroHeight = '30vh';
           break;
         case '/clothing/kaws':
-          this.backgroundImage = '../../assets/clothing-hero.jpg';
+          this.backgroundImage = 'assets/clothing-hero.jpg';
           this.page_title = 'KAWS';
           this.heroHeight = '30vh';
           break;
         case '/story':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'Our Story';
           this.heroHeight = '30vh';
           break;
         case '/shop':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'The Shop';
           this.heroHeight = '30vh';
           break;
         case '/cart':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'Cart';
           this.heroHeight = '30vh';
           break;
         case '/checkout':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'Checkout';
           this.heroHeight = '30vh';
           break;
         case '/nike/all':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'All Nikes';
           this.heroHeight = '30vh';
           break;
         case '/nike/sb':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'Nike SB';
           this.heroHeight = '30vh';
           break;
         case '/nike/supreme':
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.page_title = 'Supreme';
           this.heroHeight = '30vh';
           break;
         // Add more cases as needed
         default:
-          this.backgroundImage = '../../assets/hero.webp';
+          this.backgroundImage = 'assets/hero.webp';
           this.heroHeight = '40vh';
       }
     }
